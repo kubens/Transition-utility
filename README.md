53d7fda2ff7e97612417522a69110d88644f6e45
Jakub Laptas <kontakt@kubens.pl>
2015-12-27 00:35:01 +0100

    Added source files

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Transition-utility
+
 Get CSS transition event or call callback on CSS transition end
+
+# Usage
+
+### getEndEvent
+
+> `.getEndEvent()`
+
+Get CSS transition end event name supported for browser e.g. `oTransitionEnd` or `webkitTransitionEnd`
+
+### onEnd
+
+> `.onEnd(element, callback)`
+
+Run callback after CSS transition end specified DOM element
+
+# Example
+
+```javascript
+var transitionUtility = require('transition-utility');
+var transitionEndEvent = transitionUtility.getEndEvent();
+var domElement = document.querySelector('.transition-element');
+
+console.log(transitionEndEvent); // return transition event
+
+transitionUtility.onEnd(domElement, function () {
+	alert('End of transition');
+});
+
+```
+
+# Licence & copyright
+Transition-utility is copyright © 2015 [Jakub Laptas](http://kubens.pl) and licenced under the MIT licence. All rights not explicitly granted in the MIT license are reserved. See the included LICENSE file for more details.
---
+++ index.js
@@ -0,0 +1,55 @@
+/**
+ * Transition utility
+ * CSS Transition helper for JS
+ */
+
+
+'use strict';
+
+var bean = require('bean');
+
+module.exports = {
+	/**
+	 * Get transition event
+	 * @return {string} supported transition event
+	 */
+	getEndEvent: function () {
+		var transition;
+		var element = document.createElement('fakeelement');
+		var transitions = {
+			transition: 'transitionend',
+			OTransition: 'oTransitionEnd',
+			MozTransition: 'transitionend',
+			WebkitTransition: 'webkitTransitionEnd'
+		};
+
+		// look for supported transition
+		for (transition in transitions) {
+			if (transitions.hasOwnProperty(transition)) {
+				if (element.style[transition] !== undefined) {
+					return transitions[transition];
+				}
+			}
+		}
+
+		// transition is unsupported
+		return false;
+	},
+
+	/**
+	 * Bind transition end event to element
+	 * @param  {object} _element
+	 * @param  {function} _callback
+	 * @return {void}
+	 */
+	onEnd: function (_element, _callback) {
+		var transitionEvent = this.getEvent();
+
+		if (transitionEvent) {
+			bean.one(_element, transitionEvent, _callback);
+		}
+		else {
+			_callback.apply(this, null);
+		}
+	}
+};
